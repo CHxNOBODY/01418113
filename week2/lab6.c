945f4fa503b7c9b3457e86b539bb9a0afc971260
chxnobody <khaopan.nas@gmail.com>
2024-12-02 19:53:05 +0700

    Update lab6.c

--- week2/lab6.c
+++ week2/lab6.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
-    char ch = getchar();
-    
-    if (ch >= 'A' && ch <= 'Z') {
-        printf("Outputs: upper case\n");
-    }
-    else if (ch >= 'a' && ch <= 'z') {
-        printf("Outputs: lower case\n");
-    }
-    else if (ch >= '0' && ch <= '9') {
-        printf("Outputs: digit");
-    }
-    else if (ch == '+, - , * , /') {
-        printf("Outputs: others");
+int main() {
+    char ch;
+
+    ch = getchar(); 
+
+    if (ch >= 'a' && ch <= 'z') {
+        printf("Output: lower case\n");
+    } else if (ch >= 'A' && ch <= 'Z') {
+        printf("Output: upper case\n");
+    } else if (ch >= '0' && ch <= '9') {
+        printf("Output: digit\n");
+    } else {
+        printf("Output: others\n");
     }
 
     return 0;
