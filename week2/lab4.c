0fd07d1f3fccf90802a1e69d19b1db428d5affce
chxnobody <khaopan.nas@gmail.com>
2024-12-08 23:51:46 +0700

    Update lab4.c

--- week2/lab4.c
+++ week2/lab4.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    char salary[100];
-    
-    fgets(salary, 100, stdin);
-    double salary_int = atoi(salary);
+int main()
+{
+    char salary_str[100];
+    float salary, tax = 0.0;
 
-    if (salary_int < 0) {
-        printf("Error: Salary must be greater or equal to 0.\n");
+    fgets(salary_str, 100, stdin);
+    salary = atof(salary_str);
+
+    if (salary < 0) {
+        printf("Error: Salary must be greater or equal to 0\n");
+    } else {
+        if (salary <= 300000) {
+            tax = salary * 0.05;
+        } else {
+            tax = 300000 * 0.05 + (salary - 300000) * 0.10;
+        }
+        printf("%.2f", tax);
     }
-    else if (salary_int > 100000) {
-        printf("%d", salary_int * 0.5,salary_int);
-    }
+
+    return 0;
 }
