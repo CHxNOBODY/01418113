5ed21348585d495bbcd6a2064dcc49dcec06f7c5
chxnobody <khaopan.nas@gmail.com>
2024-12-21 16:40:05 +0700

    Update lab5.c

--- week4/lab5.c
+++ week4/lab5.c
@@ -2,38 +2,71 @@
 #include <stdlib.h>
 
 int main() {
-    char input[100];
+    char input[10];
     int n;
 
-    fgets(input, 100, stdin);
+    // Read input
+    fgets(input, 10, stdin);
     n = atoi(input);
 
-    if (n <= 0 || n > 26) { 
+    // Validate input
+    if (n <= 0 || n > 26) {
         printf("-\n");
         return 0;
     }
 
-    int size = 2 * n - 1; 
-    for (int i = 0; i < size; i++) {
-        int num_chars = (i < n) ? i : (size - i); 
-        int num_dashes = n - num_chars - 1; 
+    int width = 4 * n - 3; // Total width of the Rangoli
 
-        for (int j = 0; j < num_dashes; j++) {
+    // Generate the Rangoli pattern
+    for (int i = 0; i < n; i++) {
+        // Padding for the left side
+        for (int j = 0; j < (width - (4 * i + 1)) / 2; j++) {
             printf("-");
         }
 
-        for (int j = num_chars; j >= 0; j--) {
-            printf("%c", 'a' + (n - 1 - j));
-            if (j > 0) {
+        // Print the left side of the row
+        for (int j = 0; j <= i; j++) {
+            printf("%c", 'a' + n - 1 - j);
+            if (j < i) {
                 printf("-");
             }
         }
 
-        for (int j = 1; j <= num_chars; j++) {
-            printf("-%c", 'a' + (n - 1 - j));
+        // Print the right side of the row
+        for (int j = i - 1; j >= 0; j--) {
+            printf("-%c", 'a' + n - 1 - j);
         }
 
-        for (int j = 0; j < num_dashes; j++) {
+        // Padding for the right side
+        for (int j = 0; j < (width - (4 * i + 1)) / 2; j++) {
+            printf("-");
+        }
+
+        printf("\n");
+    }
+
+    // Generate the bottom half (mirror of the top half)
+    for (int i = n - 2; i >= 0; i--) {
+        // Padding for the left side
+        for (int j = 0; j < (width - (4 * i + 1)) / 2; j++) {
+            printf("-");
+        }
+
+        // Print the left side of the row
+        for (int j = 0; j <= i; j++) {
+            printf("%c", 'a' + n - 1 - j);
+            if (j < i) {
+                printf("-");
+            }
+        }
+
+        // Print the right side of the row
+        for (int j = i - 1; j >= 0; j--) {
+            printf("-%c", 'a' + n - 1 - j);
+        }
+
+        // Padding for the right side
+        for (int j = 0; j < (width - (4 * i + 1)) / 2; j++) {
             printf("-");
         }
 
